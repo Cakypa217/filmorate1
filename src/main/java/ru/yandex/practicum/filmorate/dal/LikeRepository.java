83d4272974d32b279267831a3cee90c9bebc4c6e
Pavel Usoltsev <usoltsevdisk@yandex.ru>
2024-11-24 22:20:27 +0300

    Замечена и исправлена недоработка в запросе id фильмов для рекомендаций. Если пересечение по лайкам с двумя пользователями одинаково, то возможно дублирование id. Добавил DISTINCT

--- src/main/java/ru/yandex/practicum/filmorate/dal/LikeRepository.java
+++ src/main/java/ru/yandex/practicum/filmorate/dal/LikeRepository.java
@@ -14,7 +14,8 @@ public class LikeRepository {
     private static final String REMOVE_LIKE = "DELETE FROM likes WHERE film_id = ? AND user_id = ?";
     private static final String UPDATE_RATE = "UPDATE films f SET rate = (SELECT COUNT(l.user_id) FROM likes l " +
             "WHERE l.film_id = f.film_id) WHERE film_id = ?";
-    private static final String GET_RECOMMENDED_FILMS_IDS = "SELECT l.film_id FROM likes l WHERE l.user_id IN " +
+    private static final String GET_RECOMMENDED_FILMS_IDS = "SELECT DISTINCT l.film_id FROM likes l " +
+            "WHERE l.user_id IN " +
             "(SELECT user_id FROM " +
                 "(SELECT l4.user_id, count(l4.user_id) common_count, max(count(l4.user_id)) OVER () max_common " +
                 "FROM likes l3 JOIN likes l4 ON l3.film_id = l4.film_id WHERE l3.user_id = ? AND l4.user_id != ? " +
